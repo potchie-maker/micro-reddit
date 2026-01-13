8325f3c3b44fc4c95e7f30f000ade6cc544f00c2
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2026-01-12 19:09:26 -0500

    Finish 'Playing with associations'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, uniqueness: true, length: { in: 4..10 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_12_223401) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_12_233859) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "link"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "password"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_12_223401) do
     t.string "username"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
